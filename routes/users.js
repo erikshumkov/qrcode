4188ccff331f58acf900a6d372a9251f6ea83834
Erik Shumkov <erikshumkov@gmail.com>
2021-05-14 22:47:03 +0200

    first

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,118 @@ router.get('/qr/:id', ensureAuthenticated, async (req, res) => {
   res.status(200).render('qr', { id })
 })
 
+// Get edit profile
+router.get('/edit/:id', ensureAuthenticated, async (req, res) => {
+  const { id } = req.params
+
+  const user = await User.findById(id).select('-password')
+
+  res.status(200).render('update', { user })
+})
+
+// Edit profile
+// router.put('/edit/:id', ensureAuthenticated, async (req, res) => {
+//   const {
+//     name,
+//     email,
+//     telephone,
+//     website,
+//     twitter,
+//     instagram,
+//     facebook,
+//     github,
+//     password,
+//     password2,
+//   } = req.body
+//   let errors = []
+
+//   // Check required fields
+//   if (!name || !email || !password || !password2) {
+//     errors.push({ msg: 'Please fill in required fields' })
+//   }
+
+//   // Check passwords match
+//   if (password !== password2) {
+//     errors.push({ msg: 'Passwords do not match' })
+//   }
+
+//   // Check passwords length
+//   if (password.length < 6) {
+//     errors.push({ msg: 'Password should be at least 6 characters' })
+//   }
+
+//   if (errors.length > 0) {
+//     res.render('update', {
+//       errors,
+//       name,
+//       email,
+//       telephone,
+//       website,
+//       twitter,
+//       instagram,
+//       facebook,
+//       github,
+//       password,
+//       password2,
+//     })
+//   } else {
+//     // Validation passed
+//     User.findOneAndUpdate({ email: email }).then(user => {
+//       if (user) {
+//         // User exists
+//         errors.push({ msg: 'Email is already registered' })
+//         res.render('register', {
+//           errors,
+//           name,
+//           email,
+//           telephone,
+//           website,
+//           twitter,
+//           instagram,
+//           facebook,
+//           github,
+//           password,
+//           password2,
+//         })
+//       } else {
+//         const newUser = new User({
+//           name,
+//           email,
+//           telephone,
+//           website,
+//           twitter,
+//           instagram,
+//           facebook,
+//           github,
+//           password,
+//         })
+
+//         // Hash password
+//         bcrypt.genSalt(10, (err, salt) =>
+//           bcrypt.hash(newUser.password, salt, (err, hash) => {
+//             if (err) throw err
+
+//             // Set password to hashed
+//             newUser.password = hash
+
+//             // Save user
+//             newUser
+//               .save()
+//               .then(user => {
+//                 req.flash(
+//                   'success_msg',
+//                   'You are now registered and can log in'
+//                 )
+//                 res.redirect('/users/login')
+//               })
+//               .catch(err => console.log(err))
+//           })
+//         )
+//       }
+//     })
+//   }
+// })
+
 // Register Handle
 router.post('/register', (req, res) => {
   const {
